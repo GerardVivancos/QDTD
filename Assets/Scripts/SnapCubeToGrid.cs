195bc304d1bb84469cb04da0440819688497c987
Gerard Vivancos <gerard.vivancos@gmail.com>
2018-06-10 01:04:54 +0200

    Cube prefab snaps to grid

---
+++ Assets/Scripts/SnapCubeToGrid.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SnapCubeToGrid : MonoBehaviour {
+
+    [SerializeField]
+    [Range(1,100)]
+    public float gridSize = 10f;
+
+	void Start () {
+        
+	}
+	
+	void Update () {
+        Vector3 p = gameObject.transform.position;
+
+        p.x = Mathf.RoundToInt(p.x / gridSize) * gridSize;
+        p.y = Mathf.RoundToInt(p.y / gridSize) * gridSize;
+        p.z = Mathf.RoundToInt(p.z / gridSize) * gridSize;
+
+        gameObject.transform.position = p;
+    }
+}
