5d10aaf04d0f5f5ef6cf2585eb25d4dfa4b6d9f4
Gerard Vivancos <gerard.vivancos@gmail.com>
2018-06-12 01:24:11 +0200

    Add World (Grid)

---
+++ Assets/Scripts/World.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour {
+
+    Dictionary<Vector2Int, GridCell> grid = new Dictionary<Vector2Int, GridCell>();
+
+	// Use this for initialization
+	void Start () {
+        LoadGrid();
+	}
+	
+    private void LoadGrid() {
+        GridCell[] cells = GetComponentsInChildren<GridCell>();
+        foreach (GridCell cell in cells) {
+            Vector2Int cellPosition = cell.GetGridPosition();
+
+            if (grid.ContainsKey(cellPosition)) {
+                Debug.LogWarning("Skipped loading of overlapping Cell in " + cellPosition);
+            } else {
+                grid.Add(cellPosition, cell);
+            }
+        }
+    }
+}
